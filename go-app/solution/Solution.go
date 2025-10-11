29ff1c687ad7e32060bdeb2a8065445be7feaf67
ganbin <jlau.ice@gmail.com>
2025-10-11 23:32:58 +0800

    init

---
+++ go-app/solution/Solution.go
@@ -0,0 +1,40 @@
+package solution
+
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+func addTwoNumbers(l1 *ListNode, l2 *ListNode) *ListNode {
+	d := &ListNode{
+		Val:  -1,
+		Next: nil,
+	}
+	m := d
+	curry := 0
+	for l1 != nil || l2 != nil || curry != 0 {
+		var a = 0
+		var b = 0
+		if l1 == nil {
+			a = 0
+		} else {
+			a = l1.Val
+			l1 = l1.Next
+		}
+		if l2 == nil {
+			b = 0
+		} else {
+			b = l2.Val
+			l2 = l2.Next
+		}
+		sum := a + b + curry
+		curry = sum / 10
+		m.Next = &ListNode{
+			Val:  sum % 10,
+			Next: nil,
+		}
+		m = m.Next
+
+	}
+	return d.Next
+}
---
+++ java/src/Solution206.java
@@ -0,0 +1,15 @@
+import model.ListNode;
+
+public class Solution206 {
+    public ListNode reverseList(ListNode head) {
+        ListNode prev = null;
+        ListNode curr = head;
+        while (curr != null) {
+            ListNode next = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = next;
+        }
+        return prev;
+    }
+}
