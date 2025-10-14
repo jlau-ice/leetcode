13a9e20b2f8542c34212656a838cc5cce4290659
ganbin <jlau.ice@gmail.com>
2025-10-14 11:41:53 +0800

    23题java解法

--- java/src/Solution.java
+++ java/src/Solution.java
@@ -97,16 +97,62 @@ public class Solution {
         return new ListNode[]{pre, head};
     }
 
+    // 23
+    public static ListNode mergeKLists(ListNode[] lists) {
+        if (lists == null || lists.length == 0) {
+            return null;
+        }
+        if (lists.length == 1) {
+            return lists[0];
+        }
+        ListNode pre = new ListNode(-1);
+        ListNode cur = pre;
+        while (!checkNull(lists)) {
+            pre.next = minHeader(lists);
+            pre = pre.next;
+        }
+        return cur.next;
+    }
+
+    public static ListNode minHeader(ListNode[] lists) {
+        int min = Integer.MAX_VALUE;
+        int minIndex = -1;
+        for (int i = 0; i < lists.length; i++) {
+            if (lists[i] == null) {
+                continue;
+            }
+            if (lists[i].val < min) {
+                min = lists[i].val;
+                minIndex = i;
+            }
+        }
+        ListNode ListNode = lists[minIndex];
+        lists[minIndex] = lists[minIndex].next;
+        return ListNode;
+    }
+
+    public static boolean checkNull(ListNode[] lists) {
+        int n = lists.length;
+        int count = 0;
+        for (ListNode cur : lists) {
+            if (cur == null) {
+                count++;
+            }
+        }
+        return count == n;
+    }
 
     public static void main(String[] args) {
-        ListNode listNode = new ListNode(1);
-        listNode.next = new ListNode(2);
-        listNode.next.next = new ListNode(3);
-        listNode.next.next.next = new ListNode(4);
-        listNode.next.next.next.next = new ListNode(5);
-        listNode.next.next.next.next.next = new ListNode(6);
-        listNode.next.next.next.next.next.next = new ListNode(7);
-        listNode.next.next.next.next.next.next.next = new ListNode(8);
-        ListNode a = reverseKGroup(listNode, 2);
+        ListNode listNode1 = new ListNode(1);
+        listNode1.next = new ListNode(2);
+        listNode1.next.next = new ListNode(3);
+        ListNode listNode2 = new ListNode(4);
+        listNode2.next = new ListNode(5);
+        listNode2.next.next = new ListNode(6);
+        ListNode listNode3 = new ListNode(7);
+        listNode3.next = new ListNode(8);
+        listNode3.next.next = new ListNode(9);
+        ListNode[] lists = new ListNode[]{listNode1, listNode2, listNode3};
+        ListNode a = mergeKLists(lists);
     }
 }
