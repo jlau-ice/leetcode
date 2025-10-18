17946136bbfca93ed2225f327d2eb8db5d56c6e9
ganbin <jlau.ice@gmail.com>
2025-10-18 23:35:36 +0800

    层序遍历

--- java/src/tree/Solution.java
+++ java/src/tree/Solution.java
@@ -65,6 +65,31 @@ public class Solution {
         preOrder(root.right, list);
     }
 
+    public List<List<Integer>> levelOrder(TreeNode root) {
+        List<List<Integer>> result = new ArrayList<>();
+        Deque<TreeNode> queue = new ArrayDeque<>();
+        if (root == null) {
+            return result;
+        }
+        queue.offer(root);
+        while (!queue.isEmpty()) {
+            int n = queue.size();
+            List<Integer> list = new ArrayList<>();
+            for (int i = 0; i < n; i++) {
+                TreeNode node = queue.poll();
+                list.add(node.val);
+                if (node.left != null) {
+                    queue.offer(node.left);
+                }
+                if (node.right != null) {
+                    queue.offer(node.right);
+                }
+            }
+            result.add(list);
+        }
+        return result;
+    }
+
     public static void main(String[] args) {
         TreeNode root = new TreeNode(15);
         root.left = new TreeNode(12);
