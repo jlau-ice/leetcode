e497fc231b586e878e97104ea23e45006599c37a
ganbin <jlau.ice@gmail.com>
2025-10-24 22:36:49 +0800

    最小栈

--- java/src/MinStack.java
+++ java/src/MinStack.java
@@ -1,4 +1,43 @@
-package PACKAGE_NAME;
+class MinStack {
 
-public class MinStack {
+    private class Node {
+        Integer val;
+        int min;
+        Node next;
+        public Node() {
+        }
+        public Node(int val, int min, Node next) {
+            this.val = val;
+            this.min = min;
+            this.next = next;
+        }
+    }
+
+    private Node root;
+
+    public MinStack() {
+        root = null;
+    }
+
+    public void push(int val) {
+        if (root != null) {
+            root = new Node(val,Math.min(val, root.min),root);
+        }else {
+            root = new Node(val,val,null);
+        }
+    }
+
+    public void pop() {
+        if (root != null) {
+            root = root.next;
+        }
+    }
+
+    public int top() {
+        return root.val;
+    }
+
+    public int getMin() {
+        return root.min;
+    }
 }
