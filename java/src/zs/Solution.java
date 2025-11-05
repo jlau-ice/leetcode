ea9e03daafcd20c774135f8a786bd7816699c6da
ganbin <jlau.ice@gmail.com>
2025-11-05 22:04:38 +0800

    最长连续子序列

--- java/src/zs/Solution.java
+++ java/src/zs/Solution.java
@@ -115,6 +115,23 @@ public class Solution {
         return list.toArray(new int[list.size()][]);
     }
 
+    public int lengthOfLIS(int[] nums) {
+        int n = nums.length;
+        int[] dp = new int[n];
+        dp[0] = 1;
+        int max = 1;
+        for (int i = 1; i < n; i++) {
+            dp[i] = 1;
+            for (int j = 0; j < i;j++) {
+                if(nums[j] < nums[i]) {
+                    dp[i] = Math.max(dp[i],dp[j] + 1);
+                }
+            }
+            max = Math.max(max,dp[i]);
+        }
+        return max;
+    }
+
     public void me(int[] nums,) {
 
 
