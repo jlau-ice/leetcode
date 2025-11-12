e8eced0e588a7d8b8dcec61fd3d3a93018fff890
ganbin <jlau.ice@gmail.com>
2025-11-12 15:25:56 +0800

    只出现一次的数字

--- java/src/Solution.java
+++ java/src/Solution.java
@@ -777,6 +777,15 @@ public class Solution {
         return ans;
     }
 
+    public int singleNumber(int[] nums) {
+        int n = nums.length;
+        int res = 0;
+        for (int i = 0; i < n; i++) {
+            res ^= nums[i];
+        }
+        return res;
+    }
+
     public static void main(String[] args) {
     }
 }
