0753b5a74b777d1bfe72a351c9613728718cfc24
ganbin <jlau.ice@gmail.com>
2025-11-15 14:52:26 +0800

    最长递增子序列

--- java/src/Solution.java
+++ java/src/Solution.java
@@ -819,6 +819,23 @@ public class Solution {
         return 0;
     }
 
+    public int lengthOfLIS(int[] nums) {
+        int n = nums.length;
+        int[] dp = new int[n];
+        dp[0] = 1;
+        int max = 1;
+        for (int i = 1; i < n; i++) {
+            dp[i] = 1;
+            for (int j = 0; j < i;j++) {
+                if(nums[j] < nums[i]) {
+                    dp[i] = Math.max(dp[i],dp[j] + 1);
+                }
+            }
+            max = Math.max(max,dp[i]);
+        }
+        return max;
+    }
+
     public static void main(String[] args) {
     }
 }
